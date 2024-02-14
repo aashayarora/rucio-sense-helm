ecc1ef1086e6bc295541026730d3a940a68330eb
aashayarora <aaarora@ucsd.edu>
2024-02-14 07:30:43 -0800

    update

--- redeploy.sh
+++ redeploy.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# Remove all previous deployment artifacts.
 helm uninstall rucio-server && helm uninstall rucio-daemons && helm uninstall rucio-db 
-kubectl delete pvc data-rucio-db-postgresql-0
+kubectl delete job rucio-server-renew-fts-proxy-on-helm-install
+kubectl delete job rucio-daemons-renew-fts-proxy-on-helm-install
 
+# Deploy Database
 ./helm_install_postgres
 sleep 30
 kubectl apply -f init-db-schema-job.yaml
 sleep 30
-./helm_install_server && ./helm_install_daemons
+./helm_install_server &
+./helm_install_daemons &

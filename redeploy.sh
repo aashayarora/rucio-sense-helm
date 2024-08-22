75dd5d2d4211752acade9b0c5ec9482fcacb256b
aashayarora <aaarora@ucsd.edu>
2024-08-22 06:48:57 -0700

    updated for nrp prod

--- redeploy.sh
+++ redeploy.sh
@@ -3,7 +3,10 @@
 export RUCIO_DB_PASS='sense-rucio'
 
 # Remove all previous deployment artifacts.
-helm uninstall rucio-server && helm uninstall rucio-daemons && helm uninstall rucio-db 
+helm uninstall rucio-server 
+helm uninstall rucio-daemons
+helm uninstall rucio-db 
+
 kubectl delete job rucio-server-renew-fts-proxy-on-helm-install
 kubectl delete job rucio-daemons-renew-fts-proxy-on-helm-install
 kubectl delete secret rucio-daemons-rucio-x509up
@@ -11,8 +14,8 @@ kubectl delete secret rucio-server-rucio-x509up
 
 # Deploy Database
 ./helm_install_postgres
-sleep 30
 kubectl apply -f init-db-schema-job.yaml
-sleep 30
+# Deploy Rucio
+sleep 50
 ./helm_install_server &
 ./helm_install_daemons &

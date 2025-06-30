b9b1b183341fb7ffe2f877ef3527b71b3da2109a
aashayarora <aaarora@ucsd.edu>
2025-06-30 16:52:11 -0700

    add scripts to add rses and dids on init

--- redeploy.sh
+++ redeploy.sh
@@ -3,19 +3,24 @@
 export RUCIO_DB_PASS='sense-rucio'
 
 # Remove all previous deployment artifacts.
-helm uninstall rucio-server 
-helm uninstall rucio-daemons
-helm uninstall rucio-db 
+helm -n ucsd-rucio uninstall rucio-server 
+helm -n ucsd-rucio uninstall rucio-daemons
+helm -n ucsd-rucio uninstall rucio-db 
 
-kubectl delete job rucio-server-renew-fts-proxy-on-helm-install
-kubectl delete job rucio-daemons-renew-fts-proxy-on-helm-install
-kubectl delete secret rucio-daemons-rucio-x509up
-kubectl delete secret rucio-server-rucio-x509up
+kubectl -n ucsd-rucio delete job rucio-server-renew-fts-proxy-on-helm-install
+kubectl -n ucsd-rucio delete job rucio-daemons-renew-fts-proxy-on-helm-install
+# kubectl -n ucsd-rucio delete secret rucio-daemons-rucio-x509up
+# kubectl -n ucsd-rucio delete secret rucio-server-rucio-x509up
 
 # Deploy Database
 ./helm_install_postgres
-kubectl apply -f init-db-schema-job.yaml
+sleep 10
+kubectl -n ucsd-rucio apply -f init-db-schema-job.yaml
 # Deploy Rucio
 sleep 50
 ./helm_install_server &
-./helm_install_daemons &
+./helm_install_daemons
+
+
+./init_rse
+./add-datasets
